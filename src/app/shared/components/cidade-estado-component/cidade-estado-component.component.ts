9ee91a0db6e7bb84fc7702793923cc1b5995eeff
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-23 16:03:25 -0300

    lint

--- src/app/shared/components/cidade-estado-component/cidade-estado-component.component.ts
+++ src/app/shared/components/cidade-estado-component/cidade-estado-component.component.ts
@@ -4,51 +4,43 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { CidadeEstadoPageComponent } from '../../../pages/cidade-estado-page/cidade-estado-page.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import {DevExtremeModule, DxFormModule, DxSelectBoxModule} from 'devextreme-angular';
+import {
+  DevExtremeModule,
+  DxFormModule,
+  DxSelectBoxModule,
+} from 'devextreme-angular';
 import { DxLoadIndicatorModule } from 'devextreme-angular/ui/load-indicator';
 import { HttpClient } from '@angular/common/http';
 import { CidadeEstadoService } from '../../services/cidade-estado.service';
 import { EstadoBr } from '../../models/estado-br.models';
 
-
-
 @Component({
   selector: 'app-cidade-estado-component',
   templateUrl: './cidade-estado-component.component.html',
-  styleUrls: ['./cidade-estado-component.component.scss']
+  styleUrls: ['./cidade-estado-component.component.scss'],
 })
 export class CidadeEstadoComponentComponent implements OnInit {
-
   estados: EstadoBr[] = [];
   cidades: CidadeBr[] = [];
 
-  estado:  EstadoBr = new EstadoBr();
+  estado: EstadoBr = new EstadoBr();
 
-
-
-
-  constructor(private http: HttpClient,
-              private cidadeEstadoService: CidadeEstadoService) { }
+  constructor(
+    private http: HttpClient,
+    private cidadeEstadoService: CidadeEstadoService
+  ) {}
 
   ngOnInit(): void {
-    this.cidadeEstadoService.getEstadosBr()
-    .subscribe( (x: EstadoBr[]) => {
+    this.cidadeEstadoService.getEstadosBr().subscribe((x: EstadoBr[]) => {
       this.estados = x;
     });
-
   }
 
-
-  onValueChangeEstado(event: any){
-    this.cidadeEstadoService.getCidadesBr(event.value)
-      .subscribe( x => {
-          this.cidades = x;
-
-        }
-      );
+  onValueChangeEstado(event: any) {
+    this.cidadeEstadoService.getCidadesBr(event.value).subscribe((x) => {
+      this.cidades = x;
+    });
   }
-
-
 }
 @NgModule({
   imports: [
@@ -57,9 +49,9 @@ export class CidadeEstadoComponentComponent implements OnInit {
     DxFormModule,
     DxLoadIndicatorModule,
     DxSelectBoxModule,
-    DevExtremeModule
+    DevExtremeModule,
   ],
-  declarations: [ CidadeEstadoComponentComponent ],
-  exports: [ CidadeEstadoComponentComponent ]
+  declarations: [CidadeEstadoComponentComponent],
+  exports: [CidadeEstadoComponentComponent],
 })
 export class CidadeEstadoComponentModule {}
